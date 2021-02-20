d50f47667b65be0974004be77eed7992befac9b6
tpo-tcmurphy <77013859+tpo-tcmurphy@users.noreply.github.com>
2021-02-20 11:10:54 -0500

    picture updates

--- app.js
+++ app.js
@@ -1,23 +1,84 @@
+/**
+ * DEPENDENCIES
+ */
 const express = require('express')
 const app = express()
-const projects = require('./data.json')
+const { projects } = require('./data.json')
 
-// Set static route and view engine to pug
-app.use('/static', express.static('public'))
+/**
+ * SET VIEW ENGINE TO PUG AND STATIC ROUTE
+ */
 app.set('view engine', 'pug')
+app.use('/static', express.static('public'))
 
-// Route Handlers
+/**
+ * ROUTE AND ERROR HANDLING
+ */
 app.get('/', (req, res) => {
-  res.render('index', projects)
+  res.render('index', { projects })
 })
 
 app.get('/about', (req, res) => {
   res.render('about')
 })
+
+// Redirects to first project if no project number is there
+app.get('/project', (req, res) => { res.redirect('/project/1') })
+
+/**
+ * Obtains user request and verifies if it is a valid project number.
+ * If valid, renders appropriate page.
+ * Creates 404 error if invalid.
+ */
 app.get('/project/:id', (req, res, next) => {
-  const projectIndex = global.parseInt(req.params.id) - 1
-  res.render('project', projects.projects[projectIndex])
+  const { id } = req.params
+  const project = projects[id - 1]
+  if (project !== undefined) {
+    res.render('project', { project })
+  } else {
+    const err = new Error()
+    err.status = 404
+    err.message = 'Woops! It seems that project is not completed yet! Why not check out some others?'
+    console.error('Project page does not exist')
+    next(err)
+  }
 })
 
-// App listener
-app.listen(3000, console.log('App is listening to port 3000'))
+/**
+ * Obtains user request at the root level and verifies if it is a valid path.
+ * Creates 404 error if invalid
+ */
+app.get('/*', (req, res, next) => {
+  const { invalidPath } = req.params
+  if (invalidPath === undefined) {
+    const err = new Error()
+    err.status = 404
+    err.message = 'Woops! It seems the page you are looking for does not exist.'
+    console.error('Page does not exist')
+    next(err)
+  } else {
+    next()
+  }
+})
+
+/**
+ * Interprets error.  If 404, displays page not found.
+ * If an error exists but is NOT a 404, sets it to 500 and displays error page.
+ */
+app.use((err, req, res, next) => {
+  if (err.status === 404) {
+    res.render('page-not-found', { err })
+  } else {
+    err.status = 500
+    err.message = 'Woah! Seems like the server had a bug. We will get an exterminator on that!'
+    console.error('Server error - are there bugs in the code that need squishing?')
+    res.render('error', { err })
+  }
+})
+
+/**
+ * SERVER RUN:
+ */
+app.listen(3000, () => {
+  console.log('The application is running on localhost: 3000')
+})
