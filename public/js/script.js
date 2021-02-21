eb4c388e5e63aaf6cc21613416cc5c457706dfb6
tpo-tcmurphy <77013859+tpo-tcmurphy@users.noreply.github.com>
2021-02-21 13:21:08 -0500

    Edited comments and changed styling

--- public/js/script.js
+++ public/js/script.js
@@ -7,6 +7,16 @@
 const body = document.querySelector('body')
 let headerBtnClicked = false
 
+document.querySelectorAll('.portfolio-intro')
+  .forEach(element => {
+    element.style.backgroundColor = '#660000'
+  })
+
+document.querySelectorAll('.portfolio-intro p')
+  .forEach(element => {
+    element.style.color = 'black'
+  })
+
 document.querySelector('#menu-icon').addEventListener('click', e => {
   !headerBtnClicked ? body.style.transform = 'translateX(300px)' : body.style.transform = 'translateX(0px)'
   return headerBtnClicked = !headerBtnClicked
